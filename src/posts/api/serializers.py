06ac19c8658df131c6e372a7a7ffb80ffa3347b7
Sergey Vanteev <fukkrypto@gmail.com>
2020-05-31 16:07:34 +0300

    добавлены Pagination,permission и url field

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -1,15 +1,24 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,HyperlinkedIdentityField
 
 from posts.models import Post
 
 
 class PostListSerializer(ModelSerializer):
+    url = HyperlinkedIdentityField(
+    view_name='posts-api:detail',
+    lookup_field='slug'
+    )
+    delete_url = HyperlinkedIdentityField(
+    view_name='posts-api:delete ',
+    lookup_field='slug'
+    )
     class Meta:
         model = Post
         fields = [
         'user',
         'title',
         'slug',
+        'url',
         'content',
         'publish'
         ]
