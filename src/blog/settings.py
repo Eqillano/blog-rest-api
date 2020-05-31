be09109874c41468aae28187275c02d06e950a6a
Sergey Vanteev <fukkrypto@gmail.com>
2020-05-31 14:21:33 +0300

    начало проекта на основе blog

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # local apps
     'comments',
     'posts',
+    'rest_framework'
 
 ]
 
@@ -142,16 +143,3 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
-
-
-
-
-
-
-
-
-
-
-
-
-
