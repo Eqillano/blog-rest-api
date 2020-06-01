136509875a78891cf31e4b8352c1b8cc1dcf7f44
Sergey Vanteev <fukkrypto@gmail.com>
2020-06-01 13:05:28 +0300

    user auth using api added

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -1,9 +1,10 @@
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
-
+from django.db.models import Q
 
 
 from rest_framework.serializers import (
+    CharField,
     EmailField,
     HyperlinkedIdentityField,
     ModelSerializer,
@@ -72,3 +73,44 @@ class UserCreateSerializer(ModelSerializer):
         user_obj.set_password(password)
         user_obj.save()
         return validated_data
+
+
+
+class UserLoginSerializer(ModelSerializer):
+    token = CharField(allow_blank=True, read_only=True)
+    username = CharField(required=False,allow_blank=True)
+    email = EmailField(label='Email Address',required=False,allow_blank=True)
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'token',
+
+        ]
+        extra_kwargs = {"password":
+                            {"write_only": True}
+                            }
+    def validate(self, data):
+        user_obj = None
+        email = data.get('email',None)
+        username = data.get('username',None)
+        password = data['password']
+        if not email and not username:
+           raise ValidationError('A user must have email')
+        user = User.objects.filter(
+        Q(email=email)|
+        Q(username=username)
+        ).distinct()
+        user = user.exclude(email__isnull=True).exclude(email__iexact='')
+        if user.exists() and user.count() == 1:
+           user_obj = user.first()
+        else:
+           raise ValidationError('This username/email is not valid.')
+
+        if user_obj:
+           if not user_obj.check_password(password):
+              raise ValidationError("incorrect password")
+
+        return data
